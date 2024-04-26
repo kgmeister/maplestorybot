c55e7d0321440ed7dd18d4a53472e39cab3e6736
agumonlyt <programmerlyt@gmail.com>
2024-04-27 00:42:17 +0800

    Update cmd_run_bot.bat

--- cmd_run_bot.bat
+++ cmd_run_bot.bat
@@ -2,6 +2,3 @@
 @echo off
 REM Start the first Python script in its directory
 start cmd /c "cd C:\Users\Screwdriver\Desktop\python\github\Bumblebee && python main.py"
-
-REM Start the second Python script in its directory
-start cmd /c "cd C:\Users\Screwdriver\Desktop\python\github\BumblebeeRune && python main.py" 
